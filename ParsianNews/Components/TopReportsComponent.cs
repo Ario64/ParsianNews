04245292ab3310c9acfa1e366d6ac49ee7327b47
Ario64 <shahrokhi20@hayoo.com>
2025-01-29 10:41:32 +0330

    top report added

---
+++ ParsianNews/Components/TopReportsComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.CodeDom;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParsianNews.Data;
+using ParsianNews.Models;
+using ReportGroup = ParsianNews.Utilities.ReportGroup;
+
+namespace ParsianNews.Components;
+
+public class TopReportsComponent : ViewComponent
+{
+    private readonly ParsianNewsDbContext _context;
+
+    public TopReportsComponent(ParsianNewsDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Report>? Reports { get; set; }
+
+    public async Task<IViewComponentResult> InvokeAsync(string groupName = ReportGroup.MultiMedia, int take = 4)
+    {
+        Reports =await _context.Reports
+            .Where(w => w.ReportGroup!.GroupName != groupName)
+            .OrderByDescending(q => q.ReportView)
+            .Take(take)
+            .ToListAsync();
+        return View("/Pages/Components/_TopReports.cshtml", Reports);
+    }
+}
--- ParsianNews/Utilities/ReportGroup.cs
+++ ParsianNews/Utilities/ReportGroup.cs
@@ -5,7 +5,6 @@ namespace ParsianNews.Utilities;
 public static class ReportGroup
 {
     public const string Sport = "ورزشی";
-    public const string Technology = "تکنولوژی";
     public const string Health = "سلامت";
     public const string Social = "اجتماعی";
     public const string Artistic = "هنری";
