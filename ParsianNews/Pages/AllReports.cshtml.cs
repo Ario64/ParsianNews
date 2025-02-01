85f6f4880499b3f5b51055b2d47bbc0d09d23551
Ario64 <shahrokhi20@hayoo.com>
2025-02-01 11:21:17 +0330

    paging completed in all sport reports

--- ParsianNews/Pages/AllReports.cshtml.cs
+++ ParsianNews/Pages/AllReports.cshtml.cs
@@ -17,12 +17,36 @@ namespace ParsianNews.Pages
 
         public IEnumerable<Report>? Reports { get; set; }
 
-        public async Task<ActionResult> OnGetAsync(string group)
+        public async Task<ActionResult> OnGetAsync(string group, int pageId = 1)
         {
+            int take = 2;
+            int skip = (pageId - 1) * take;
+
+            int count = _context.Reports
+                .Include(i => i.ReportGroup)
+                .Count(c => c.ReportGroup!.GroupName == group);
+
+            ViewData["Count"] = count;
+            ViewData["Take"] = take;
+            ViewData["PageId"] = pageId;
+            ViewData["PreviousPage"] = pageId - 1;
+            ViewData["NextPage"] = pageId + 1;
+
+            if (count % 2 == 0)
+            {
+                ViewData["PageCount"] = (count / take);
+            }
+            else
+            {
+                ViewData["PageCount"] = (count / take) + 1;
+            }
+
             Reports = await _context.Reports
                 .Include(i => i.ReportGroup)
                 .Where(w => w.ReportGroup!.GroupName == group)
                 .OrderByDescending(o => o.CreateDate)
+                .Skip(skip)
+                .Take(take)
                 .ToListAsync();
 
             ViewData["GroupName"] = group;
